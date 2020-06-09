ba1b2fa65e05c66d0ca397c9c21b323a52f4ed77
heyong <coolhy123@163.com>
2020-06-09 16:38:01 +0800

    动态从数据库获取数据配置权限喝控制

--- src/main/java/com/hydu/entity/User.java
+++ src/main/java/com/hydu/entity/User.java
@@ -1,13 +1,9 @@
 package com.hydu.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 
 import org.springframework.security.core.userdetails.UserDetails;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
@@ -24,35 +20,61 @@ import java.util.List;
 @Data
 public class User implements UserDetails {
 
-
+    /**
+     * 用户id
+     */
     private Integer id;
 
 
+    /**
+     * 用户名
+     */
     private String username;
 
-
+    /**
+     * 用户真实姓名
+     */
     private String userRealname;
 
-
+    /**
+     * 用户密码
+     */
     private String password;
 
-
+    /**
+     * 创建时间
+     */
     private Date createDate;
 
-
+    /**
+     * 最后登录时间
+     */
     private Date lastLoginTime;
 
-
+    /**
+     * 是否可用
+     */
     private boolean enabled;
 
-
+    /**
+     *账户是否过期
+     */
     private boolean accountNonExpired;
 
-
+    /**
+     *账户是否被锁定
+     */
     private boolean accountNonLocked;
 
-
+    /**
+     * 证数是否过期
+     */
     private boolean credentialsNonExpired;
+
+    /**
+     * 加密盐
+     */
+    private String salt;
     /**
      * 用户权限
      */
